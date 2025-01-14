bc65bc3c35d1827df4b9cc168b4083d1ecd1a091
lucas-nelson-uiuc <lucas.nelson.contacts@gmail.com>
2025-01-13 22:44:41 -0600

    add bowlers, sorting methods

--- src/pinsdb/bowl/bowlers.py
+++ src/pinsdb/bowl/bowlers.py
@@ -7,6 +7,18 @@ class Bowler:
     bowler_id: str
     nicknames: Sequence[str]
 
+    def __lt__(self, other: "Bowler") -> bool:
+        return self.bowler_id < other.bowler_id
+    
+    def __le__(self, other: "Bowler") -> bool:
+        return self.bowler_id <= other.bowler_id
+    
+    def __gt__(self, other: "Bowler") -> bool:
+        return self.bowler_id > other.bowler_id
+    
+    def __ge__(self, other: "Bowler") -> bool:
+        return self.bowler_id >= other.bowler_id
+
 
 Alek = Bowler(bowler_id="Alek", nicknames=("A", "AL", "ALE", "ALK", "ALEK", "POP"))
 Cam = Bowler(bowler_id="Cam", nicknames=("C", "CA", "CAM", "CAMERON"))
@@ -17,8 +29,10 @@ Naomi = Bowler(bowler_id="Naomi", nicknames=("N", "NAOMI"))
 Ryley = Bowler(bowler_id="Ryley", nicknames=("R", "RY", "RYL", "RYLEY"))
 Sara = Bowler(bowler_id="Sara", nicknames=("SARA"))
 Spencer = Bowler(bowler_id="Spencer", nicknames=("S", "SPE", "SPO", "SPEN", "SPENCER"))
+Karly = Bowler(bowler_id="Karly", nicknames=("POO",))
+Ryan = Bowler(bowler_id="Ryan", nicknames=("PEE",))
 
-registered_bowlers = (
+registered_bowlers = sorted(
     Alek,
     Cam,
     Drew,
@@ -28,4 +42,6 @@ registered_bowlers = (
     Ryley,
     Sara,
     Spencer,
+    Karly,
+    Ryan
 )
