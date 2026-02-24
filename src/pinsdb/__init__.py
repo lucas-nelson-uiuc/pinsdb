c0e972be9bb51ec50f541eb289902f63cfbc1986
lucas-nelson-uiuc <lucas.nelson.contacts@gmail.com>
2026-02-24 17:30:44 -0600

    fix: typo

--- src/pinsdb/__init__.py
+++ src/pinsdb/__init__.py
@@ -1,8 +1,3 @@
-from pinsdb import (
-    bowl as bowl,
-    db as db,
-    namespace as namespace,
-    viz as viz
-)
+from pinsdb import bowl as bowl, namespace as namespace, viz as viz
 
 __version__ = "0.1.0"
